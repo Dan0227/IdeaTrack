562c5ac7d68f112d333492febc07cae2e78bbdab
Dan0227 <Jhojan0227@gmail.com>
2026-03-28 17:15:07 -0500

    Added --watch to dev script and port confi in server.js

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,1 +1,9 @@
-console.log("Online");
+import express from 'express';
+
+const app = express();
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
